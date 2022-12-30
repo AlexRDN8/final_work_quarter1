dfe82e5328761269d9230c810c9ae315c6766d39
AlexRDN8 <alexrodin8@bk.ru>
2022-12-30 14:21:59 +0300

    added final task

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекоммендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello","2","world",":-)"] -> ["2",":-)"]
+
+string[] array = new string []{"hello", "2", "world", ":-)"};
+int lengthNewArray = GetLengthNewArray(array);
+string[] newArray = GetArrayWith4LessSigns(array, lengthNewArray);
+PrintArray(newArray);
+
+int GetLengthNewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {        
+        if (array[i].Length <= 3)
+        {
+            count +=1;
+        }
+    }
+    return count;
+}
+
+GetArrayWith4LessSigns(array, lengthNewArray);
+
+string[] GetArrayWith4LessSigns(string[] oldArray, int lengthNewArray)
+{
+    string[] newArray = new string [lengthNewArray];
+    int index = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= 3)
+        {
+            newArray[index] = oldArray[i];
+            index +=1;
+        }    
+    }
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    string message = string.Join(", ", array);
+    Console.WriteLine(message);
+}
+
+
